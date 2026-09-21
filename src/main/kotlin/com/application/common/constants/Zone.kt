bfc22b724e3ddb3152412624fbd34ee9db54c08f
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2026-09-21 18:24:41 -0300

    fix: add missing newline at end of Zone.kt file

--- src/main/kotlin/com/application/common/constants/Zone.kt
+++ src/main/kotlin/com/application/common/constants/Zone.kt
@@ -3,6 +3,6 @@ package com.application.common.constants
 import java.time.ZoneId
 
 object Zone {
-    const val AMERICA_SAO_PAULO =  "America/Sao_Paulo"
-    val ID_AMERICA_SAO_PAULO =  ZoneId.of(AMERICA_SAO_PAULO)
+    const val AMERICA_SAO_PAULO = "America/Sao_Paulo"
+    val ID_AMERICA_SAO_PAULO = ZoneId.of(AMERICA_SAO_PAULO)
 }
